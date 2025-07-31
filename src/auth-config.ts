c9af34ea4670d508d61fe122284d479cb0c35a50
Gaurav <gauravyadav199808@gmail.com>
2025-07-31 11:29:53 +0530

    Resovled env bug

--- src/auth-config.ts
+++ src/auth-config.ts
@@ -1,10 +1,12 @@
 import { Configuration, LogLevel } from "@azure/msal-node";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const config: Configuration = {
   auth: {
-    clientId: process.env.CLIENT_ID!,
-    authority: `https://login.microsoftonline.com/${process.env.TENANT_ID!}`,
-    clientSecret: process.env.CLIENT_SECRET!,
+    clientId: process.env.AZURE_CLIENT_ID!,
+    authority: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID!}`,
+    clientSecret: process.env.AZURE_CLIENT_SECRET!,
   },
   system: {
         loggerOptions: {
