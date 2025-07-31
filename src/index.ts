75e7f6c52fba2b2eac26772aef755900194860ea
Gaurav <gauravyadav199808@gmail.com>
2025-07-31 15:16:37 +0530

    Added cors and resolved bugs

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ import session from 'express-session';
 import { ConfidentialClientApplication, AuthorizationCodeRequest } from '@azure/msal-node';
 import {config} from './auth-config.ts';
 import { authenticatedGraphClient } from './graph-helper.ts';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
+
+app.use(express.json());
+app.use(cors({
+  origin: process.env.CLIENT_URL! || "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, 
+  }));
 
 app.use(session({
     secret: process.env.SESSION_SECRET!,
@@ -24,20 +32,21 @@ app.use("/auth/signin", async (req, res)=>{
     const authCodeUrlParameters = {
         scopes: ["Calendars.ReadWrite", "Calendars.ReadWrite.Shared"],
         redirectUri: process.env.AZURE_REDIRECT_URI!,
+        prompt: "consent" 
     }
     try {
         const authUrl = await msalInstance.getAuthCodeUrl(authCodeUrlParameters);
         res.redirect(authUrl);
     } catch (error) {
         console.error("Error getting auth code URL:", error);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send("Internal Server Error from /auth/signin");
     }
 })
 
 app.use("/auth/callback", async (req, res) => {
      const tokenRequest: AuthorizationCodeRequest = {
         code: req.query.code as string,
-        scopes: ["Calendars.Read"],
+        scopes: ["Calendars.ReadWrite", "Calendars.ReadWrite.Shared"],
         redirectUri: process.env.AZURE_REDIRECT_URI!,
     };
     try {
@@ -47,7 +56,7 @@ app.use("/auth/callback", async (req, res) => {
         res.redirect("/calendar")
     } catch (error) {
         console.error("Error acquiring token by code:", error);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send("Internal Server Error from /auth/callback");
     }
 })
 
